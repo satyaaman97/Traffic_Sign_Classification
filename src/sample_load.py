767396c906bf2b3e1e6e2c6c19eb43534fbf11f8
seki2928 <57238231+seki2928@users.noreply.github.com>
2019-11-13 10:03:41 -0700

    Sample source code for handling pickle file
    It uses ToySet1.pickle to load.

---
+++ src/sample_load.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 16:42:08 2019
+
+@author: Joon Kim
+"""
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = 'ToySet1.pickle'
+# Opening 'pickle' file and getting images
+with open(file, 'rb') as f:
+    d = pickle.load(f, encoding='latin1')  # dictionary type, we use 'latin1' for python3
+    # At the same time method 'astype()' used for converting ndarray from int to float
+    # It is needed to divide float by float when applying Normalization
+    x_train = d['x_train'].astype(np.float32)
+    y_train = d['y_train']
+    x_validation = d['x_validation'].astype(np.float32)
+    y_validation = d['y_validation']
+    x_test = d['x_test'].astype(np.float32)
+    y_test = d['y_test']
+
+print('Sample Images')
+
+for i in range(10):
+    plt.imshow(x_train[i, 0, :, :].astype(np.uint8), cmap=plt.get_cmap('gray'))
+    plt.show()
+
+print('x_train.shape: ', x_train.shape)
+print('y_train.shape: ', y_train.shape)
+print('x_validation.shape: ', x_validation.shape)
+print('y_validation.shape: ', y_validation.shape)
+print('x_test.shape: ', x_test.shape)
+print('y_test.shape: ', y_test.shape)
+
+del d
